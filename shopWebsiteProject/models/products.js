0756b0fdc21f53e5f4c14c9cd178a41eaef8e493
joytrinh <mongtuyen149@gmail.com>
2020-01-04 21:22:55 +0700

    Complete section 7 Model View Controller MVC

--- shopWebsiteProject/models/products.js
+++ shopWebsiteProject/models/products.js
@@ -1,38 +1,34 @@
 const fs = require('fs')
 const path = require('path')
-
+const p = path.join(path.dirname(process.mainModule.filename), 
+        'data', 
+        'products.json'
+    )
+const getProductsFromFile = cb => {
+    
+    fs.readFile(p,(err,fileContent) =>{
+        if(err) {
+            cb([])
+        } else {
+            cb(JSON.parse(fileContent))
+        }   
+    })
+}
 module.exports = class Product {
     constructor(t){//create an object based on this class where I can pass a title into the constructor
         this.title = t
     }
     //This is a function but no "function" keyword
     save(){//store products in an array and fetch it
-        const p = path.join(path.dirname(process.mainModule.filename), 
-        'data', 
-        'products.json'
-        ) //'data' is the name of a folder, 'products.json' we will store product in this file
-
-        fs.readFile(p,(err, fileContent) => {
-            let products = []
-            if(!err){
-                products = JSON.parse(fileContent)
-            }
-            products.push(this) //this = class Product
-        })
-
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-            console.log(err)
+        getProductsFromFile(products =>{
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
         })
     }
 
     static fetchAll(cb){//static = I can call this method directly on the class itself and not on an instantiated obj
-        const p = path.join(path.dirname(process.mainModule.filename), 
-        'data', 
-        'products.json'
-        )
-        fs.readFile(p,(err,fileContent) =>{
-            if(err) cb([]) // we do not need an "else" because after return [], it will stop execution
-            cb(JSON.parse(fileContent))
-        })
+        getProductsFromFile(cb)
     }
 }
