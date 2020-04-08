15cd68a1649536649df93206e5eab39bf829e995
joytrinh <mongtuyen149@gmail.com>
2020-04-08 22:17:00 +0700

    Add an order, add relational order data, get orders, Finish MongoDB section

--- shopWebsiteProject/controllers/shop.js
+++ shopWebsiteProject/controllers/shop.js
@@ -1,19 +1,21 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
-  .then(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
+    .then((products) => {
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products",
+      });
     })
-  })
-  .catch(err => {console.log(err)})
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId
+  const prodId = req.params.productId;
   //Method 1:
   // Product.findAll({where: {id: prodId}})
   //       .then(([products]) => {
@@ -26,105 +28,88 @@ exports.getProduct = (req, res, next) => {
   //       .catch(err => console.log(err))
   //Method 2:
   Product.findByPk(prodId)
-        .then(product=>{
-          res.render('shop/product-detail', {
-            product: product,//in sequelize, there is no array
-            pageTitle: product.title,
-            path: '/products'
-          })
-        })
-        .catch(err => console.log(err))
-}
+    .then((product) => {
+      res.render("shop/product-detail", {
+        product: product, //in sequelize, there is no array
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll()
-  .then(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+    .then((products) => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+      });
     })
-  })
-  .catch(err => {console.log(err)})
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.getCart = (req, res, next) => {
   req.user
-  .getCart()
-  .then(products => {
-    res.render('shop/cart', {
-      path: '/cart',
-      pageTitle: 'Your Cart',
-      products: products
+    .getCart()
+    .then((products) => {
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
+      });
     })
-  })
-  .catch(err => {console.log(err)})
-  }
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId
+  const prodId = req.body.productId;
   Product.findByPk(prodId)
-  .then(product => {
-    return req.user.addToCart(product)
-  })
-  .then(result => {
-    res.redirect('/cart')
-  })
-}
+    .then((product) => {
+      return req.user.addToCart(product);
+    })
+    .then((result) => {
+      res.redirect("/cart");
+    });
+};
 
 exports.postCartDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId
+  const prodId = req.body.productId;
   req.user
-  .deleteItemFromCart(prodId)
-  .then(result => {
-    res.redirect('/cart')
-  })
-  .catch(err => {console.log(err)})
-}
-exports.postOrder = (req, res, next) => {
-  let fetchedCart
-  req.user.getCart()
-  .then(cart => {
-    fetchedCart = cart
-    return cart.getProducts()
-  })
-  .then(products => {
-    return req.user
-    .createOrder()
-    .then(order => {
-      return order.addProducts(
-        products.map(product => {
-          product.orderItem = {quantity: product.cartItem.quantity}
-          return product
-        })
-      )
+    .deleteItemFromCart(prodId)
+    .then((result) => {
+      res.redirect("/cart");
     })
-    .catch(err => {console.log(err)})
-  })
-  .then(result => {
-    return fetchedCart.setProducts(null)//remove all of items in the cart
-  })
-  .then(result => {
-    res.redirect('/orders')
-  })
-  .catch(err => {console.log(err)})
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
+exports.postOrder = (req, res, next) => {
+  req.user
+    .addOrder()
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 exports.getOrders = (req, res, next) => {
   req.user
-  .getOrders({include: ['products']})
-  /*if you are fetching all the orders, please also fetch all related products already 
-  and give me back one array of orders that also includes the products per order.
-  Now this only works of course because we do have a relation between orders and products
-  as set up in app.js here and now we can load both together.
-  Each order will now have a products array
-  */
-  .then(orders => {
-    res.render('shop/orders', {
-      path: '/orders',
-      pageTitle: 'Your Orders',
-      orders: orders//the orders variable which simply stores all the retrieved orders. So with that I got my orders for this user
+    .getOrders()
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders, //the orders variable which simply stores all the retrieved orders. So with that I got my orders for this user
+      });
     })
-  })
-  .catch(err => {console.log(err)})
-  
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
--- shopWebsiteProject/models/user.js
+++ shopWebsiteProject/models/user.js
@@ -1,103 +1,140 @@
-const mongodb = require('mongodb')
-const ObjectId = mongodb.ObjectId
-const getDb = require('../util/database').getDb
+const mongodb = require("mongodb");
+const ObjectId = mongodb.ObjectId;
+const getDb = require("../util/database").getDb;
 
 class User {
-    constructor(username, email, cart, id){
-        this.username = username
-        this.email = email
-        this.cart = cart // {items: []}
-        this._id = id
-    }
+  constructor(username, email, cart, id) {
+    this.username = username;
+    this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
+  }
 
-    save(){
-        const db = getDb()
-        return db.collection('users')
-                .insertOne(this)
-    }
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
 
-    addToCart(product){
-      
-      const cartProductIndex = this.cart.items.findIndex(cp => {
-        return cp.productId.toString() === product._id.toString();
-      });
-      let newQuantity = 1;
-      const updatedCartItems = [...this.cart.items];
-        if (cartProductIndex >= 0) {
-          newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-          updatedCartItems[cartProductIndex].quantity = newQuantity;
-        }
-        else {
-          updatedCartItems.push({
-            productId: new ObjectId(product._id),
-            quantity: newQuantity
-          })//the javascript spread operator, three dots(...) to pull 
-            //out all properties of this object so of the product object and then with a comma, you can add or overwrite a property,
-        }
-        const updatedCart = {
-          items: updatedCartItems
-        };
-        const db = getDb();
-        /*
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      }); //the javascript spread operator, three dots(...) to pull
+      //out all properties of this object so of the product object and then with a comma, you can add or overwrite a property,
+    }
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+    const db = getDb();
+    /*
         I'll just set cart equal to updated cart, that is it. So cart which I expect to have in a user in the database will
         now receive updated cart, so this object as a new value which will overwrite the old one and this is important,
         it will not merge this with the old one, it will not merge the elements in the items array, it will simply overwrite 
         the old cart with the new cart.
         */
-       return db
-       .collection('users')
-       .updateOne(
+    return db
+      .collection("users")
+      .updateOne(
         { _id: new ObjectId(this._id) },
         { $set: { cart: updatedCart } }
       );
-    }    
+  }
 
-    getCart() {
-        const db = getDb()
-        const productIds = this.cart.items.map(i => {
-          return i.productId//make an array of id
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId; //make an array of id
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } }) //find many products: $in takes an array of id
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
         });
-        return db
-          .collection('products')
-          .find({ _id: { $in: productIds } })//find many products: $in takes an array of id
-          .toArray()
-          .then(products => {
-            return products.map(p => {
-              return {
-                ...p,
-                quantity: this.cart.items.find(i => {
-                  return i.productId.toString() === p._id.toString();
-                }).quantity
-              }
-            })
-          })
-      }
+      });
+  }
 
-      deleteItemFromCart(productId) {
-        const updatedCartItems = this.cart.items.filter(item => { //make a new array
-          return item.productId.toString() !== productId.toString(); //this return true if we wanna keep in the array and false if we wanna get rid of it
-        })//toString() to get the right datatype
-        const db = getDb()
-        return db.collection('users')
-                    .updateOne(
-                      { _id: new ObjectId(this._id) },
-                      { $set: { cart: {items: updatedCartItems} } }
-                    );
-    }
-    static findByPk(userId){
-        const db = getDb()
-        return db.collection('users')
-        .findOne({ _id: new ObjectId(userId) })//return a cursor
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      //make a new array
+      return item.productId.toString() !== productId.toString(); //this return true if we wanna keep in the array and false if we wanna get rid of it
+    }); //toString() to get the right datatype
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
+
+  // Add an order in users and clear the cart
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        // We need a snapshot because we do not want to change anything in past order
+
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            username: this.username,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] };
+        return db
+          .collection("users")
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "user._id": new ObjectId(this._id) })
+      .toArray();
+  }
+
+  static findByPk(userId) {
+    const db = getDb();
+    return (
+      db
+        .collection("users")
+        .findOne({ _id: new ObjectId(userId) }) //return a cursor
         // .next()//so we have to use next()
         //If you're sure that there is only ONE result, we can use findOne and don't need to use next()
-        .then(user => {
-            return user;
-          })
-          .catch(err => {
-            console.log(err);
-          });
-    }
+        .then((user) => {
+          return user;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    );
+  }
 }
 
-
-module.exports = User
+module.exports = User;
