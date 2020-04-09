abd0c5914e3ea70788c22be6eff0d399bef3b022
joytrinh <mongtuyen149@gmail.com>
2020-04-09 22:25:00 +0700

    Start with mongoose

--- shopWebsiteProject/app.js
+++ shopWebsiteProject/app.js
@@ -1,39 +1,45 @@
-const path = require('path')
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
-const errorController = require('./controllers/error')
-const mongoConnect = require('./util/database').mongoConnect//which is a function
-const User = require('./models/user')
+const app = express();
 
-const app = express()
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 //register a new middleware to store that user in my request so that I can use it everywhere
 //when the app starts, it will run sequelize firstly. This middleware only run when there is an incoming request
 app.use((req, res, next) => {
-    User.findByPk("5e8bed2d211812138bdf00a8") 
-    .then(user => {
-        req.user = new User(user.username, user.email, user.cart, user._id)//store the user in the request from the database, then we can use req.user
-        //we create a new User(), so we can access User's methods        
-        next()
+  User.findByPk("5e8bed2d211812138bdf00a8")
+    .then((user) => {
+      req.user = new User(user.username, user.email, user.cart, user._id); //store the user in the request from the database, then we can use req.user
+      //we create a new User(), so we can access User's methods
+      next();
     })
-    .catch(err => { console.log(err) })
-})
-app.use('/admin', adminRoutes)
-app.use(shopRoutes) 
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.get404)
+app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000)
-})
+mongoose
+  .connect(
+    "mongodb+srv://tuyentrinh:tuyen1234@cluster0-7dvwp.azure.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
--- shopWebsiteProject/models/product.js
+++ shopWebsiteProject/models/product.js
@@ -1,65 +1,88 @@
-const mongodb = require('mongodb')
-const getDb = require('../util/database').getDb
-
-class Product {
-  constructor(title, price, description, imageURL, id, userId){
-    this.title = title
-    this.price = price
-    this.description = description
-    this.imageURL = imageURL
-    this._id = id ? new mongodb.ObjectId(id) : null
-    this.userId = userId
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema //constructor to make a schema
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageURL: {
+    type: String,
+    required: true
   }
+})
 
-  save(){
-    const db = getDb()
-    let dbOp
-    if (this._id) {
-      //Update the product
-      dbOp = db.collection('products')
-      .updateOne({_id: this._id}, {$set: this})//if you wanna update any properties, you can write {$set: {title: this.title}, {}, {}}
-    } else {
-      //Insert new product
-      dbOp = db.collection('products').insertOne(this)
-    }
-    return dbOp    
-    .then(result => {
-      console.log(result)
-    })
-    .catch(err => {console.log(err)})
-  }
 
-  static fetchAll(){
-    const db = getDb()
-    return db.collection('products')
-    .find()
-    .toArray()
-    .then(products => {
-      return products;
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  }
 
-  static findByPk(prodId){
-    const db = getDb()
-    return db.collection('products')
-    .find({_id: new mongodb.ObjectId(prodId)})//Because mongoDB store data in bson format like this: _id:ObjectId(5e676425dcfc1521aa77e59d)
-    .next()
-    .then(product => {
-      return product;
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  }
+// const mongodb = require('mongodb')
+// const getDb = require('../util/database').getDb
 
-  static deleteById(prodId){
-    const db = getDb()
-    return db.collection('products')
-    .deleteOne({_id: new mongodb.ObjectId(prodId)})
-  }
-}
+// class Product {
+//   constructor(title, price, description, imageURL, id, userId){
+//     this.title = title
+//     this.price = price
+//     this.description = description
+//     this.imageURL = imageURL
+//     this._id = id ? new mongodb.ObjectId(id) : null
+//     this.userId = userId
+//   }
 
-module.exports = Product
+//   save(){
+//     const db = getDb()
+//     let dbOp
+//     if (this._id) {
+//       //Update the product
+//       dbOp = db.collection('products')
+//       .updateOne({_id: this._id}, {$set: this})//if you wanna update any properties, you can write {$set: {title: this.title}, {}, {}}
+//     } else {
+//       //Insert new product
+//       dbOp = db.collection('products').insertOne(this)
+//     }
+//     return dbOp    
+//     .then(result => {
+//       console.log(result)
+//     })
+//     .catch(err => {console.log(err)})
+//   }
+
+//   static fetchAll(){
+//     const db = getDb()
+//     return db.collection('products')
+//     .find()
+//     .toArray()
+//     .then(products => {
+//       return products;
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+//   }
+
+//   static findByPk(prodId){
+//     const db = getDb()
+//     return db.collection('products')
+//     .find({_id: new mongodb.ObjectId(prodId)})//Because mongoDB store data in bson format like this: _id:ObjectId(5e676425dcfc1521aa77e59d)
+//     .next()
+//     .then(product => {
+//       return product;
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+//   }
+
+//   static deleteById(prodId){
+//     const db = getDb()
+//     return db.collection('products')
+//     .deleteOne({_id: new mongodb.ObjectId(prodId)})
+//   }
+// }
+
+// module.exports = Product
