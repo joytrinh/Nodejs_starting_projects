ca7dd1216a7bd0dcb04d748d89ad2c5dca170d9c
joytrinh <mongtuyen149@gmail.com>
2020-04-12 22:30:32 +0700

    Use mongodb to store session

--- shopWebsiteProject/app.js
+++ shopWebsiteProject/app.js
@@ -2,11 +2,17 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session) //session is a variable above
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
-
+const MONGODB_URL = "mongodb+srv://tuyentrinh:tuyen1234@cluster0-7dvwp.azure.mongodb.net/shop?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URL,
+  collection: 'sessions'
+})
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -17,6 +23,7 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}))
 
 //register a new middleware to store that user in my request so that I can use it everywhere
 //when the app starts, it will run sequelize firstly. This middleware only run when there is an incoming request
@@ -40,7 +47,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://tuyentrinh:tuyen1234@cluster0-7dvwp.azure.mongodb.net/shop?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+    MONGODB_URL
   )
   .then(result => {
     User.findOne().then(user => {
--- shopWebsiteProject/controllers/auth.js
+++ shopWebsiteProject/controllers/auth.js
@@ -1,13 +1,13 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get('Cookie').trim().split('=')[1]//get the value true of the cookie => true
+  console.log(req.session.isLoggedIn)
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true')
+  req.session.isLoggedIn = true
   res.redirect("/");
 };  
